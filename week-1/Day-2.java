f690c24ba578f01ad721a3aacddb6252530939f0
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-11-23 06:41:31 +0530

    Min- max Sum

---
+++ week-1/Day-2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+    // Write your code here
+        Collections.sort(arr);
+        long midSum = 0L;
+        for (int i = 1;i<4;i++) midSum +=arr.get(i);
+        System.out.println((midSum + (long) arr.get(0)) +" "+ (midSum + (long) arr.get(4)));
+    }
+
+}
+
+public class MiniMaxSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
